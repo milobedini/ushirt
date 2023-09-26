16b7f2f9da74a73904cb384bb1d284b82bd4cab8
Milo Bedini <milobedini64@gmail.com>
2023-09-26 18:57:01 +0100

    tabs and layout for customiser screen

--- frontend/src/canvas/index.jsx
+++ frontend/src/canvas/index.jsx
@@ -1,5 +1,5 @@
 const Canvas = () => {
-  return <div>index</div>;
+  return <div></div>;
 };
 
 export default Canvas;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,7 @@
+import AIPicker from './AIPicker';
+import ColourPicker from './ColourPicker';
+import CustomButton from './CustomButton';
+import FilePicker from './FilePicker';
+import Tab from './Tab';
+
+export { AIPicker, ColourPicker, CustomButton, FilePicker, Tab };
